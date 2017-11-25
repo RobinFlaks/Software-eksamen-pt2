9a431e5ff6095dc573bd7f8cfd545fd31a471d06
amidav16 <davidamirth@gmail.com>
2017-11-25 14:32:04 +0100

    Unit Test Update
    #btw

---
+++ Eksamen/BazzarTesting/BasicPackTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Eksamen;
+
+namespace BazzarTesting
+{
+    [TestClass]
+    public class BasicPackTest
+    {
+        [TestMethod]
+        public void GetDescription_Returns_DefaultString()
+        {
+
+			//Test if the default value is equal to " with a Rare card"
+
+			//Arrange
+			var BasePack = new BasicPack();
+			//Act
+			var Result = BasePack.GetDescription();	
+			//Assert
+			Assert.AreEqual(Result, " with a Rare card");
+        }
+    }
+}
